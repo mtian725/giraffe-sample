05aa92031c2257b8788119601f56b041ab6be6ae
Merrick Tian <mtian@ip-192-168-1-118.ec2.internal>
2021-06-07 14:29:41 -0400

    updated gitignore

--- src/env.js
+++ src/env.js
@@ -1,11 +1,11 @@
 /** InfluxDB v2 URL */
-const url = process.env['INFLUX_URL'] || 'http://localhost:8086'
+const url = process.env['INFLUX_URL'] || 'https://us-east-1-1.aws.cloud2.influxdata.com'
 /** InfluxDB authorization token */
-const token = process.env['INFLUX_TOKEN'] || 'my-token'
+const token = process.env['INFLUX_TOKEN'] || '6juAAqWKpDi7WO9lBT4qkcxVba36hrZPlGXRgzJB26bh3OOuaR_K4LxLJX9b5ZvGToYtC-NH26OyUhOTO0OcRQ=='
 /** Organization within InfluxDB  */
-const org = process.env['INFLUX_ORG'] || 'my-org'
+const org = process.env['INFLUX_ORG'] || '882f0952c1216374'
 /**InfluxDB bucket used in examples  */
-const bucket = 'my-bucket'
+const bucket = 'mtian\'s Bucket'
 
 module.exports = {
   url,
